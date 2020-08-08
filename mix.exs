3fa80e7d414a2e210c5b59064e16c471e9da46a1
Marcel Fahle <m.fahle@gmail.com>
2020-08-08 17:28:47 +0300

    adds liveview dropzone, removes clutter

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LiveViewDropzoneTest.MixProject do
     [
       app: :live_view_dropzone_test,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -46,7 +46,8 @@ defmodule LiveViewDropzoneTest.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_live_view_dropzone, "~> 0.0.7"}
     ]
   end
 
